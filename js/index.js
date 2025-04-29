2faa6758de0b0384fafaf32a88b9e4b5ff9ad000
Nikita Simonov <ngsimonov@yandex.ru>
2025-04-29 21:22:43 +0500

    feat: small popup done

--- js/index.js
+++ js/index.js
@@ -2,14 +2,46 @@ try {
 window.addEventListener('scroll', () => {
     let Header = document.getElementById("header");
     if (window.scrollY < 100) {
-        console.log("Открываем!");
         Header.className = "header-wrapper";
     } else {
-        console.log("Прячем!");
         Header.classList.add("scrolled-active");
+    }
+
+    if (document.getElementById("small-popup").style.display == 'flex') {
+        document.getElementById("small-popup").style.display = 'none';
+        document.getElementById("popup-closer").style.display = 'none';
     }
 });
 
+} catch (error) {
+    console.error(error);
+}
+
+let CurrentButton = 0;
+
+function SmallPopUp(button) {
+    let ButtonElement = document.getElementById('offer-' + button);
+    let position = ButtonElement.getBoundingClientRect();
+
+    const x = position.left + window.scrollX;
+    document.getElementById("small-popup").style.left = x + 'px';
+    document.getElementById("small-popup").style.display = 'flex';
+    document.getElementById("popup-closer").style.display = 'block';
+    CurrentButton = button;
+}
+
+function CloseSmallPopUp() {
+    document.getElementById("small-popup").style.display = 'none';
+    document.getElementById("popup-closer").style.display = 'none';
+}
+
+try {
+    window.addEventListener('resize', () => {
+        let ButtonElement = document.getElementById('offer-' + CurrentButton);
+        let position = ButtonElement.getBoundingClientRect();
+        const x = position.left + window.scrollX;
+        document.getElementById("small-popup").style.left = x + 'px';
+    });
 } catch (error) {
     console.error(error);
 }
